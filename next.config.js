ba5f97300cce75e1c8aefa1d8e0720b079076896
rael5 <rael1up@gmail.com>
2025-01-14 11:43:04 -0400

    Tailwind not working?

--- next.config.js
+++ next.config.js
@@ -1,9 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  images: {
-    domains: ['s4.anilist.co'],
-  },
+reactStrictMode: true,
+images: {
+domains: ['s4.anilist.co'],
+},
+plugins: [require('tailwindcss')],
 }
 
 module.exports = nextConfig
