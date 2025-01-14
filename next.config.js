48ef677787d2cca50ba83ea9dc79434ac4d3a7f1
rael5 <rael1up@gmail.com>
2025-01-14 10:27:40 -0400

    updating next.config.js

--- next.config.js
+++ next.config.js
@@ -4,13 +4,6 @@ const nextConfig = {
   images: {
     domains: ['s4.anilist.co'],
   },
-  webpack: (config) => {
-    config.module.rules.push({
-      test: /\.css$/,
-      use: ['postcss-loader'],
-    });
-    return config;
-  },
 }
 
 module.exports = nextConfig
