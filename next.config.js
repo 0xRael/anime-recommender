4cf6a71df884e07ce0df7bf1afb0f53b1de33984
rael5 <rael1up@gmail.com>
2025-01-14 10:26:26 -0400

    updating next.config.js

--- next.config.js
+++ next.config.js
@@ -1,5 +1,16 @@
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
-};
+  reactStrictMode: true,
+  images: {
+    domains: ['s4.anilist.co'],
+  },
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.css$/,
+      use: ['postcss-loader'],
+    });
+    return config;
+  },
+}
 
-export default nextConfig;
+module.exports = nextConfig
